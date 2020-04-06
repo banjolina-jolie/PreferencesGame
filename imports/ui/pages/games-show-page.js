534ac7baca073176fd214ff48c7b6ed3a678c79e
Dylan O'Carroll <dylancocarroll@gmail.com>
2020-04-06 16:49:49 -0700

    text formatting

--- imports/ui/pages/games-show-page.js
+++ imports/ui/pages/games-show-page.js
@@ -7,59 +7,49 @@ import './games-show-page.html';
 
 Template.Games_show_page.onCreated(function gamesShowPageOnCreated() {
   this.getGameId = () => FlowRouter.getParam('_id');
-  this.autorun(() => {
-    // this.playerName = prompt('what is your name?');
-    // Games.update(this.getGameId, {$set: { playerNames }});
-  });
-});
-
-Template.Games_show_page.onDestroyed(function gamesShowPageOnDestroyed() {
-
+  this.getCurrentGame = () => Games.findOne({ _id: this.getGameId() });
 });
 
 Template.Games_show_page.helpers({
-  game() {
-    return Games.findOne({ _id: this.getGameId() });
-  },
   selectedEntries() {
     const gameId = Template.instance().getGameId();
     return Entries.find({ gameId, selected: true }, { sort: [['rank']]});
-  },
-  entries() {
-    const gameId = Template.instance().getGameId();
-    return Entries.find({ gameId });
   },
   entriesLength() {
     const gameId = Template.instance().getGameId();
     return Entries.find({ gameId, selected: null }).count();
   },
+  players() {
+    const gameId = Template.instance().getGameId();
+    const game = Games.findOne({ _id: gameId });
+
+    if (game && game.playerNames) {
+      game.playerNames = JSON.parse(game.playerNames);
+      return game.playerNames.filter(p => !!p);
+    }
+  }
 });
 
 Template.Games_show_page.events({
   'submit .new-entry'(event) {
-    // Prevent default browser form submit
     event.preventDefault();
 
-    // Get value from form element
     const text = $('#new-entry-input').val();
 
     if (!text) return;
 
     const gameId = Template.instance().getGameId();
 
-    // Insert a entry into the collection
     Entries.insert({
       gameId,
       text,
-      createdAt: new Date(), // current time
+      createdAt: new Date(),
     });
 
-    // Clear form
     $('#new-entry-input').val('');
   },
 
   'submit .new-round'(event) {
-    // Prevent default browser form submit
     event.preventDefault();
 
     const gameId = Template.instance().getGameId();
