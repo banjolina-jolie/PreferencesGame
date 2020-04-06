b923ec9254bc6980d62bd98694542c95aecb21e7
Dylan O'Carroll <dylancocarroll@gmail.com>
2020-04-05 22:24:43 -0700

    sorting

--- imports/api/entries.js
+++ imports/api/entries.js
@@ -2,16 +2,14 @@ import { Mongo } from 'meteor/mongo';
 
 export const Entries = new Mongo.Collection('entries');
 
-// export function removeSelectedEntries(gameId) {
-//   return Entries.remove({ gameId, selected: true });
-// }
-
 if (Meteor.isServer) {
   Meteor.startup(function() {
     return Meteor.methods({
+
       removeSelectedEntries: function(gameId) {
         return Entries.remove({ gameId, selected: true });
       },
+
       setNewSelected: function(gameId) {
         const entries = Entries.find({ gameId }).fetch();
 
@@ -20,19 +18,50 @@ if (Meteor.isServer) {
 
         // Get sub-array of first n elements after shuffled
         const selected = shuffled.slice(0, 4).map(x => x._id);
-        Entries.update({
-          gameId,
-          _id: { $in: selected },
-        },
-        {
-          $set: {
-            selected: true,
+
+        selected.forEach((entryId, idx) => {
+          Entries.update({
+            gameId,
+            _id: entryId,
           },
-        },
-        {
-          multi: true,
+          {
+            $set: {
+              selected: true,
+              rank: idx + 1,
+            },
+          })
         });
-      }
+      },
+
+      changeEntryRank: function({ gameId, entryId, rank }) {
+        const entries = Entries.find({ gameId, selected: true }).fetch();
+        let sortedByRank = [];
+        let selectedEntry;
+
+        entries.forEach((entry, idx) => {
+          sortedByRank[entry.rank - 1] = entry;
+        });
+
+        for (let i in sortedByRank) {
+          if (sortedByRank[i]._id === entryId) {
+            selectedEntry = sortedByRank.splice(i, 1)[0];
+          }
+        }
+
+        sortedByRank.splice(rank-1, 0, selectedEntry);
+
+        sortedByRank.forEach((entry, idx) => {
+          Entries.update({
+            _id: entry._id
+          },
+          {
+            $set: {
+              rank: idx + 1,
+            },
+          })
+        });
+      },
+
     });
   });
 
--- imports/ui/pages/games-show-page.js
+++ imports/ui/pages/games-show-page.js
@@ -7,12 +7,8 @@ import './games-show-page.html';
 
 Template.Games_show_page.onCreated(function gamesShowPageOnCreated() {
   this.getGameId = () => FlowRouter.getParam('_id');
-  // console.log(this.getGameId());
-  this.game = Games.findOne({ _id: this.getGameId() });
-  // console.log(this.game);
   this.autorun(() => {
     // this.playerName = prompt('what is your name?');
-    // console.log(this.game);
     // Games.update(this.getGameId, {$set: { playerNames }});
   });
 });
@@ -23,11 +19,11 @@ Template.Games_show_page.onDestroyed(function gamesShowPageOnDestroyed() {
 
 Template.Games_show_page.helpers({
   game() {
-    return  Games.findOne({ _id: this.getGameId() });
+    return Games.findOne({ _id: this.getGameId() });
   },
   selectedEntries() {
     const gameId = Template.instance().getGameId();
-    return Entries.find({ gameId, selected: true });
+    return Entries.find({ gameId, selected: true }, { sort: [['rank']]});
   },
   entries() {
     const gameId = Template.instance().getGameId();
@@ -71,4 +67,16 @@ Template.Games_show_page.events({
     Meteor.call('removeSelectedEntries', gameId);
     Meteor.call('setNewSelected', gameId);
   },
+
+  'click .change-rank'(event) {
+    const gameId = Template.instance().getGameId();
+    const { rank, entryId } = $(event.target).data();
+
+    Meteor.call('changeEntryRank', {
+      gameId,
+      entryId,
+      rank,
+    });
+  },
+
 });
