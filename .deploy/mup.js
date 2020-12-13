ae9ef5265d5341211607920757fb2105ff5a92dd
George Kontridze <george.kontridze@gmail.com>
2020-12-13 13:06:30 -0800

    Use Meteor Up

---
+++ .deploy/mup.js
@@ -0,0 +1,34 @@
+const process = require('process');
+
+module.exports = {
+  servers: {
+    one: {
+      host: '54.201.138.24',
+      username: 'admin',
+      pem:`${process.env.HOME}/.ssh/dylan-and-george.pem`
+    }
+  },
+  app: {
+    name: 'preferences-game',
+    path: '../',
+    docker: {
+      image: 'abernix/meteord:node-12-base',
+    },
+    servers: {
+      one: {},
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    env: {
+      ROOT_URL: 'http://preferences.dylanocarroll.com',
+      MONGO_URL: 'mongodb://localhost:27017/meteor',
+    },
+  },
+  mongo: {
+    version: '4.4.2',
+    servers: {
+      one: {}
+    }
+  },
+};
